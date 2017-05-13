f3de2fcf59f210cb026ff3d755fa59631794bf2e
Florentin Hennecker <florentin.hennecker@gmail.com>
2017-05-13 12:55:52 +0200

    [add] split and save dataset

--- preprocessing.py
+++ preprocessing.py
@@ -4,6 +4,7 @@ import sys, os
 import string
 import re
 import random
+import numpy as np
 
 CLASSES = string.digits + string.ascii_uppercase + string.ascii_lowercase
 
@@ -18,10 +19,28 @@ def load_filenames(datapath, filters=[]):
             filenames += list(map(lambda f : path+'/'+f, files))
     return filenames
 
+def split_and_save_dataset(dataset, filename):
+    """Splits a dataset in 3 and saves lists of filenames."""
+    splits = [0.6, 0.2, 0.2]
+    split_names = ['train', 'validation', 'test']
+    perm = np.random.permutation(len(dataset))
+    
+    for s, split in enumerate(splits):
+        startindex = int(sum(splits[:s]) * len(dataset))
+        endindex = int(startindex + splits[s] * len(dataset))
+        with open(filename+'_'+split_names[s], 'w') as f:
+            for i in perm[startindex:endindex]:
+                f.write(dataset[i]+'\n')
+
+    with open(filename, 'w') as f:
+        for name in dataset:
+            f.write(name+'\n')
+
 def get_class_index(filename):
     return int(re.findall(r'.*img(\d+).*', filename)[0])-1
 
 def get_class(filename):
+    """Get the actual digit or character of the image"""
     return CLASSES[get_class_index(filename)]
 
 
@@ -32,5 +51,4 @@ if __name__ == "__main__":
     opt = parser.parse_args()
     
     filenames = load_filenames(opt.datapath, ['Good', 'Bmp'])
-    for f in random.sample(filenames, 10):
-        print(f, get_class_index(f), get_class(f))
+    split_and_save_dataset(filenames, 'good')
